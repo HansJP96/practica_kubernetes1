db2d9c1a7be76fe6f836819bc73fa02186f73fc6
Hansee Jimenez <hansee1996@gmail.com>
2024-08-03 17:09:50 -0500

    Cambio test devops

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -12,7 +12,7 @@ public class BasicApplicationTests {
  
  @Test
  public void contextLoads() {
- 	    String meessage= "default message cambio test devops";
+ 	    String meessage= "default message cambio por mi nombre Hansee";
  	    Assert.assertNotNull(meessage);
  }
  
