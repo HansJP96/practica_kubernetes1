38a0b7f277fc4bc808a41d96ebe8694592459357
Hansee Jimenez <hansee1996@gmail.com>
2024-08-08 17:32:32 -0500

    se limpian imports y se agrega readme info

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # practica_kubernetes1
 hola
-prueba merge
+
+mirr pr activos
++refs/pull/*/head:refs/remotes/origin/pr/*
+
+current_status = $.action
+pr_title = $.pull_request.title
+
+
+case "$current_status" in
+    opened|reopened|synchronize)
+        echo "New PR created: $pr_title"
+        echo "Build"
+        # Ejecuta los comandos necesarios aquí, por ejemplo:
+        cd billing
+        mvn clean test //tambien puede ser compile
+        ;;
+    *)
+        echo "Condition not met: Skipping this step."
+        exit 1
+        ;;
+esac
+
+sonar.projectKey=sonarqube
+sonar.sources=billing/src/main/java
+sonar.java.binaries=billing/target/classes
--- billing/src/main/java/com/paymentchain/billing/common/InvoiceResposeMapper.java
+++ billing/src/main/java/com/paymentchain/billing/common/InvoiceResposeMapper.java
@@ -5,13 +5,14 @@
  */
 package com.paymentchain.billing.common;
 
-import com.paymentchain.billing.dto.InvoiceResponse;
-import com.paymentchain.billing.entities.Invoice;
 import java.util.List;
-import org.mapstruct.InheritInverseConfiguration;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
+
+import com.paymentchain.billing.dto.InvoiceResponse;
+import com.paymentchain.billing.entities.Invoice;
 
 /**
  *
@@ -19,20 +20,20 @@ import org.mapstruct.Mappings;
  */
 @Mapper(componentModel = "spring")
 public interface InvoiceResposeMapper {
-    
-      @Mappings({
-  @Mapping(source = "customerId", target = "customer"),
-  @Mapping(source = "id", target = "invoiceId")})
-  InvoiceResponse InvoiceToInvoiceRespose(Invoice source);  
-  
-  
-  
-  List<InvoiceResponse> InvoiceListToInvoiceResposeList(List<Invoice> source);    
 
-/*  @InheritInverseConfiguration
-  Invoice InvoiceResponseToInvoice(InvoiceResponse srr);
-  
-  @InheritInverseConfiguration
-  List<Invoice> InvoiceResponseToInvoiceList(List<InvoiceResponse> source);  */  
-    
+  @Mappings({
+      @Mapping(source = "customerId", target = "customer"),
+      @Mapping(source = "id", target = "invoiceId") })
+  InvoiceResponse InvoiceToInvoiceRespose(Invoice source);
+
+  List<InvoiceResponse> InvoiceListToInvoiceResposeList(List<Invoice> source);
+
+  /*
+   * @InheritInverseConfiguration
+   * Invoice InvoiceResponseToInvoice(InvoiceResponse srr);
+   * 
+   * @InheritInverseConfiguration
+   * List<Invoice> InvoiceResponseToInvoiceList(List<InvoiceResponse> source);
+   */
+
 }
